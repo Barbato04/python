eff43f4067f220a68aa9dd709253795807c8959e
Barbato04 <109222422+Barbato04@users.noreply.github.com>
2022-08-03 11:14:52 -0300

    pp

---
+++ Ejercicio1.py
@@ -0,0 +1,18 @@
+#-*-coding:utf-8 -*-
+#Decoracion: Nombre del Algoritmo
+print("--------------------------------------------")
+print("Ejerciocio1: CALCULAR DISTANCIA")
+print("--------------------------------------------")
+
+#Entradas
+
+print ("ingrese la velocidad y el tiempo de unidad movil")
+#covirtiendo entrada a entero
+V = float(imput("Velocidad:"))
+T = int(input("Tiempo:"))
+
+#Proceso
+D = V*T
+
+#Salida
+print(D)
